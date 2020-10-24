c4cc2edb9dd4b60554dc727872485731c75dceac
taisiia-filikova <filikova@gmail.com>
2020-10-24 17:08:44 +0300

    Вносит файлы сборки и разметку

---
+++ build/main.511cf0c3a3f2f3f9bf43.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{QfWi:function(i,n,w){"use strict";w.r(n)}},[["QfWi",1]]]);
+//# sourceMappingURL=main.511cf0c3a3f2f3f9bf43.js.map
---
+++ src/index.js
@@ -0,0 +1,2 @@
+import './styles.css';
+import './js/array-of-colors';
